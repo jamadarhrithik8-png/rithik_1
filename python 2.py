d5b393abaafa004881f76d4e9c2be0724e0a4d08
Rithik <jamadarhrithik8@gmail.com>
2026-04-07 08:58:50 +0530

    Add files via upload

---
+++ python 2.py
@@ -0,0 +1,10 @@
+name = input("Enter name: ")
+year_of_birth = int(input("Enter year of birth: "))
+
+current_year = 2026
+age = current_year - year_of_birth
+
+if age >= 60:
+    print(name, "is a Senior Citizen")
+else:
+    print(name, "is not a Senior Citizen")
